1789f08521c394ff63794e506c96e3aa6eea0b6b
tejas1154 <tejas.ramane@aurionpro.com>
2025-07-04 09:09:47 +0530

    Add files via upload

---
+++ AccountConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AccountClassLib;
+
+namespace AccountConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account("Tejas", "12355", 3000);
+
+            Console.WriteLine("Initial Details: ");
+            Console.WriteLine(a);
+
+            a.Deposit(2000);
+            Console.WriteLine("\nAfter Deposit Details: ");
+            Console.WriteLine(a);
+
+            bool result = a.Withdraw(2000); 
+            Console.WriteLine(result ? "\nWithdrawal successfull" : "\nwithdrawal failed");
+            Console.WriteLine(a);
+
+        }
+    }
+}
